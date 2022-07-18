5b07c9f16d489278f9d3c9b2f0a1016b95fa8733
prabhakara BM <prabhakar.mun488@gmail.com>
2022-07-18 12:12:44 +0530

    notication teacher side code

--- src/main/java/Pages/NotificationPage.java
+++ src/main/java/Pages/NotificationPage.java
@@ -1,23 +1,11 @@
 package Pages;
 
-import java.time.Duration;
-import java.util.Calendar;
 import java.util.List;
-import java.util.Random;
-import java.util.UUID;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import io.opentelemetry.exporter.logging.SystemOutLogExporter;
 
 
 
